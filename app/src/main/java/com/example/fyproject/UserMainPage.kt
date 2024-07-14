5abe5f0c8bab6cdd5e6f0e63d6b860f5ce0f51cc
Asus <1181203214@student.mmu.edu.my>
2024-07-14 21:09:50 +0800

    visitor list

--- app/src/main/java/com/example/fyproject/UserMainPage.kt
+++ app/src/main/java/com/example/fyproject/UserMainPage.kt
@@ -1,18 +1,22 @@
 package com.example.fyproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.fyproject.databinding.ActivityMainBinding
 import com.example.fyproject.databinding.ActivityUserMainPageBinding
+import com.google.android.material.navigation.NavigationView
 
-class UserMainPage : AppCompatActivity() {
+
+class UserMainPage : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityUserMainPageBinding
     private lateinit var toggle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,26 +28,69 @@ class UserMainPage : AppCompatActivity() {
 
             setSupportActionBar(toolbar)
 
-            toggle = ActionBarDrawerToggle(this@UserMainPage, myDrawerLayout, R.string.nav_open, R.string.nav_close)
-
+            toggle = ActionBarDrawerToggle(
+                this@UserMainPage,
+                myDrawerLayout,
+                toolbar,
+                R.string.nav_open,
+                R.string.nav_close
+            )
+            toggle.isDrawerIndicatorEnabled = true
             myDrawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
 
-            navView.setNavigationItemSelectedListener {
-                when(it.itemId){
-                    R.id.home_nav_menu -> {
-                        Toast.makeText(this@UserMainPage,"Home is selected", Toast.LENGTH_SHORT).show()
-                    }
+            navView.setNavigationItemSelectedListener(this@UserMainPage)
 
-                    R.id.profile_nav_menu -> {
-                        Toast.makeText(this@UserMainPage,"Profile is selected", Toast.LENGTH_SHORT).show()
-                    }
+            setToolbarTitle("Home")
 
+        }
 
-                }
-                true
+//                when(it.itemId){
+//                    R.id.home_nav_menu -> {
+//                        setToolbarTitle("Home")
+//                        changeFragment(UserMainPage())
+//                        Toast.makeText(this@UserMainPage,"Home is selected", Toast.LENGTH_SHORT).show()
+//                        myDrawerLayout.closeDrawer(GravityCompat.START)
+//
+//                        return true
+//                    }
+//
+//                    R.id.visitorList_nav_menu -> {
+//                        setToolbarTitle("Visitor List")
+//                        Toast.makeText(this@UserMainPage,"visitor List is selected", Toast.LENGTH_SHORT).show()
+//
+//                        val Fragment = VisitorListFragment()
+//                        val fragmentTransaction = supportFragmentManager.beginTransaction()
+//                        fragmentTransaction.replace(R.id.visitorListLayout, Fragment) // Replace with replace if needed
+//                        fragmentTransaction.commit()
+//
+//                    }
+//
+//                    R.id.profile_nav_menu -> {
+//                        Toast.makeText(this@UserMainPage,"Profile is selected", Toast.LENGTH_SHORT).show()
+//                    }
+//
+//
+//                }
+//                true
+            }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.home_nav_menu -> {
+                setToolbarTitle("Home")
+                var intent = Intent(this, UserMainPage::class.java)
+                startActivity(intent);
+            }
+
+            R.id.visitorList_nav_menu -> {
+                setToolbarTitle("Visitor List")
+                changeFragment(VisitorListFragment())
             }
         }
 
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -55,4 +102,18 @@ class UserMainPage : AppCompatActivity() {
         }
 
     }
+
+    fun setToolbarTitle(title:String){
+        supportActionBar?.title = title
     }
+
+    fun changeFragment(frag: Fragment){
+        val fragment = supportFragmentManager.beginTransaction()
+        fragment.replace(R.id.fragmentContainer, frag).commit()
+    }
+
+}
+
+
+
+
