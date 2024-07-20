558318ebce54190bb38c8b1a57e742a1f38b2ad0
MiaoOAO <josantan123@gmail.com>
2024-07-20 14:50:19 +0800

    branch12 datePicker in visitorregistrationFragement done

--- app/src/main/java/com/example/fyproject/VisitorRegistrationFragment.kt
+++ app/src/main/java/com/example/fyproject/VisitorRegistrationFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fyproject
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +14,7 @@ import androidx.appcompat.app.AlertDialog
 import com.example.fyproject.databinding.ActivityRegistrationBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Calendar
 import java.util.Date
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,6 +31,7 @@ class VisitorRegistrationFragment : Fragment() {
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var fStore: FirebaseFirestore
+    private lateinit var datePickerDialog: DatePickerDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,19 +46,45 @@ class VisitorRegistrationFragment : Fragment() {
         val vBtn:Button = view.findViewById(R.id.visSubmitBtn)
         val sBtn:Button = view.findViewById(R.id.searchVisBtn)
 
+
+        //Date Picker by using dialog method
+        val dateLabel = view.findViewById<TextView>(R.id.visDateTv)
+        var selectedDate: String? = null
+        val calendar = Calendar.getInstance()
+        val initialYear = calendar.get(Calendar.YEAR)
+        val initialMonth = calendar.get(Calendar.MONTH)
+        val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        datePickerDialog = DatePickerDialog(requireContext(),
+            { view, year, monthOfYear, dayOfMonth ->
+                // Handle selected date (update label, perform actions)
+                selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
+
+                dateLabel?.text = "Selected Date: $selectedDate"
+            },
+            initialYear, initialMonth, initialDay
+        )
+
+        // (Optional) Set onClick listener on a button to show the dialog
+        val selectDateButton = view.findViewById<Button>(R.id.visDateBtn)
+        selectDateButton.setOnClickListener {
+            datePickerDialog.show()
+        }
+
+
 //      add visitor registration to firestore
         vBtn.setOnClickListener{
 
             val vName = view.findViewById<TextView?>(R.id.visName).text.toString()
             val vPlate = view.findViewById<TextView?>(R.id.visPlateNo).text.toString()
             val vPhone = view.findViewById<TextView?>(R.id.visPhone).text.toString()
-            val vDate = view.findViewById<TextView?>(R.id.visDate).text.toString()
+//            val vDate = view.findViewById<TextView?>(R.id.visDate).text.toString()
 
             val visitorMap = hashMapOf(
                 "name" to vName,
                 "plateNo" to vPlate,
                 "phone" to vPhone,
-                "VisitDate" to vDate
+                "VisitDate" to selectedDate
             )
 
             val userId = FirebaseAuth.getInstance().currentUser!!.uid
@@ -75,6 +104,7 @@ class VisitorRegistrationFragment : Fragment() {
             //search vehicle plate function
             checkPlateNumberDialog()
         }
+
 
 
 
